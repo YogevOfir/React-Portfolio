bb1f819ed211366b479077bc38b251e5e35caff9
YogevOfir <yogev282000@gmail.com>
2025-08-12 20:18:55 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,75 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I'm a recent graduate in Computer Science and Mathematics with a passion for building impactful technology. I thrive on solving challenging problems, learning quickly, and continuously improving. With a detail-oriented mindset and strong communication skills, I'm eager to contribute to innovative projects and grow as a developer.`;
+
+export const ABOUT_TEXT = `As a versatile full-stack developer, I specialize in creating efficient and user-friendly applications for both mobile and web platforms. My technical toolkit includes React Native, Flutter, Java, C++, Python, along with experience in frameworks like React, Node.js, and databases such as FireBase and MongoDB. I enjoy working across the stack—from designing intuitive interfaces to building robust backend systems—and I'm always exploring new technologies to expand my capabilities. Whether collaborating with a team or working independently, I bring a self-driven approach and a commitment to delivering high-quality solutions.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - 2025",
+    role: "Computer Science & Mathematics B.sc",
+    company: "Ariel University",
+    description: `Completed a Computer Science degree with a strong foundation in software development, algorithms, and system architecture, gaining hands-on experience through academic projects and collaborative problem-solving.`,
+    technologies: ["Flutter", "FireBase", "Python", "Java", "C", "CPP", "SQL", "MongoDB"],
+  },
+  {
+    year: "2025 - 2025",
+    role: "Meta React Specialization",
+    company: "Coursera - Meta",
+    description: `Completed the Meta React Specialization through Coursera, mastering front-end development with React, including component-based architecture, state management, and responsive design principles.`,
+    technologies: ["React.js", "JavaScript"],
+  },
+  {
+    year: "2022 - 2022",
+    role: "Deep Learning Specialization",
+    company: "Coursera",
+    description: `Completed the Deep Learning Specialization on Coursera, gaining expertise in neural networks, convolutional and recurrent architectures, and practical applications of deep learning in real-world scenarios.`,
+    technologies: ["Python", "TensorFlow", "Pytorch", "Keras", "NumPy"],
+  },
+  {
+    year: "2018 - 2021",
+    role: "Deputy Class Commander ",
+    company: "IDF",
+    description: `Developed leadership and technical expertise in an artillery battery by managing classified equipment, optimizing command-level communication, and operating the SHEDER HAM system under high-pressure conditions.`,
+    technologies: ["SHEDER-HAM"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "TechNaim - Real-Time Technician Tracking & Scheduling",
+    image: project1,
+    description:
+      "A real-time technician tracking and scheduling system designed to optimize field operations, enhance workforce coordination, and improve service delivery efficiency.",
+    technologies: ["React-Native", "Type-Script", "CSS", "JavaScript", "Node.js", "MongoDB", "Express.js"],
+  },
+  {
+    title: "Semantic Template Matching for UAV Localization",
+    image: project2,
+    description:
+      "A research project focused on enhancing unmanned aerial vehicle positioning accuracy using semantic image analysis and template matching techniques.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "TailwindCSS"],
+  },
+  {
+    title: "NoteOverTheWorld",
+    image: project4,
+    description:
+      "A location-based note-taking app that allows users to save notes and visualize them on an interactive map using Google Maps integration.",
+    technologies: ["Flutter", "FireBase"],
+  },
+];
+
+export const CONTACT = {
+  phoneNo: "053-531-0520 ",
+  email: "yogev282000@gmail.com",
+};
