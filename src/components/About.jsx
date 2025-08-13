86d9027a10a51e1e3881af61ef24babbecf9e9fd
YogevOfir <yogev282000@gmail.com>
2025-08-13 11:46:34 +0300

    images removed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <span className="text-neutral-500"> Me</span>
     </h2>
     <div className="flex flex-wrap">
-        <motion.div 
+        {/* <motion.div 
          whileInView={{opacity: 1, x: 0 }}
          initial={{opacity: 0, x: -100}}
          transition={{duration: 0.5}}
@@ -17,7 +17,7 @@ const About = () => {
             <div className="flex items-center justify-center">
                 <img className="rounded-2xl" src={aboutImg} alt="about" />
             </div>  
-        </motion.div>
+        </motion.div> */}
         <motion.div 
          whileInView={{opacity: 1, x: 0 }}
          initial={{opacity: 0, x: 100}}
