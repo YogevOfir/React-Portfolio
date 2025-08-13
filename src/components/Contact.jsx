dee60d8e97bf3bc72d339190c81c8ee92202b773
YogevOfir <yogev282000@gmail.com>
2025-08-13 12:04:38 +0300

    added animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,14 +12,6 @@ const Contact = () => {
             Get In Touch
         </motion.h2>
         <div className="text-center tracking-tighter">
-            {/* <motion.p
-                whileInView={{ opacity: 1, x: 0 }}
-                initial={{ opacity: 0, x: -100 }}
-                transition={{ duration: 1 }}
-                className="my-4"
-            >
-                {CONTACT.address}
-            </motion.p> */}
             <motion.p
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
@@ -28,9 +20,15 @@ const Contact = () => {
             >
                 {CONTACT.phoneNo}
             </motion.p>
-            <a href={`mailto:${CONTACT.email}`} className="border-b" aria-label="Send email">
+            <motion.a 
+             whileInView={{ opacity: 1, x: 0 }}
+             initial={{ opacity: 0, x: -100 }}
+             transition={{ duration: 1 }}
+             href={`mailto:${CONTACT.email}`} 
+             className="border-b" aria-label="Send email"
+            >
                 {CONTACT.email}
-            </a>
+            </motion.a>
         </div>
     </div>
 }
