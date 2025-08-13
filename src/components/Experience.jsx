d0b65b098a73e71122356cfbc13b4b319fe2998e
YogevOfir <yogev282000@gmail.com>
2025-08-13 11:28:18 +0300

    fix title

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,7 @@ const Experience = () => {
          transition={{duration: 0.5}}
          className="my-20 text-center text-4xl"
         >
-            Experience
+            Experience & Education
         </motion.h2>
         <div>
             {EXPERIENCES.map((experience, index) => (
